5f194d2c4a570b0ccd9f8cf46e6eaa23f2947cd9
hedas <hedas@10.0.0.133>
2023-04-10 10:46:40 -0700

    initial commit

---
+++ src/day2Package1/Day2Test1.java
@@ -0,0 +1,12 @@
+package day2Package1;
+
+public class Day2Test1 {
+	public static void main(String[] args) {
+		System.out.println("testing.....");
+	}
+
+	public void test() {
+		System.out.println("my stash test");
+	}
+	
+}
